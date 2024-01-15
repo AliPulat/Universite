eaeb48ecd17366e1ee4c8c770bd4912553a4426e
AliPulat <147386215+AliPulat@users.noreply.github.com>
2024-01-15 06:26:38 +0300

    Add files via upload

---
+++ syf92_odv2/syf92_odv2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syf92_odv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, a, e = 1;
+            int[] B = new int[10];
+            for(int i = 0; i < B.Length; i++)
+            {
+                Console.Write((i + 1) + ". Sayıyı Giriniz = ");
+                B[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (a = 2; a < B.Length; a++)
+            {
+                if (Math.Abs(B[0] - B[a]) < Math.Abs(B[0] - B[e])) e = a;
+            }
+            Console.WriteLine("İlk Sayıya En Yakın Sayı = " + B[e]);
+            Console.ReadKey();
+        }
+    }
+}
