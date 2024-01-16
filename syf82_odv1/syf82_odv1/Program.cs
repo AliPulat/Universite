dcf8eeb517bdc484856269e7cf4bedd5f7674065
AliPulat <147386215+AliPulat@users.noreply.github.com>
2024-01-16 20:26:51 +0300

    Add files via upload

---
+++ syf82_odv1/syf82_odv1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syf82_odv1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//aldığı burs 500TL altında olanlara %10 ve +50 TL sabit artış yapan program!
+         //burs miktarları a dizi değişkeninde saklanıyor! foreach ile yapımı 
+
+            double[] burs = { 150, 600, 700, 450, 850, 275, 950, 1500, 210, 368 };
+            
+
+            foreach (double a in burs)
+            {
+               
+                if(a >= 500)
+                {
+                    Console.WriteLine(a);
+                    continue;
+                }
+
+
+                double islem = (a * 0.1) + a;
+                islem += 50;
+                Console.WriteLine(islem);
+            }
+
+            Console.Write(" \n Sonuçlar Yukarıda Ki Gibidir");
+            Console.ReadKey();
+        }
+    }
+}
